510310aeabe74c2c8a708afa7fd5442627abaaae
dertimA <amadoreleoncio@gmail.com>
2025-10-07 19:29:10 +0800

    5

--- src/js/config.js
+++ src/js/config.js
@@ -2,6 +2,8 @@ const getApiBaseUrl = () => {
     const hostname = window.location.hostname;
 
     if (hostname === '127.0.0.1' || hostname === 'localhost') {
+        return 'http://192.168.100.12:5000';
+    } else {
         return 'https://nodefibear.onrender.com';
     }
 };
