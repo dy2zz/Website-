891c1d664b36b1f9836e204a81b82634b23d942f
dertimA <amadoreleoncio@gmail.com>
2025-10-07 18:57:34 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/templates/**/*.html", // Scans all HTML files in the templates folder
-    "./src/js/**/*.js",         // Scans all JS files in the js folder
-  ],
+  "./*.html",        // Scans all HTML files in the root
+  "./js/**/*.js",    // Scans all JS files in js folder
+],
   theme: {
     extend: {},
   },
